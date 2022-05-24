9b640b7456ddddd3d0bcda4b41c804506c52f074
Ed <ed@guiness.me>
2022-05-24 09:38:41 +0100

    .

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log('App is running');
+
+function showImg(which) {
+    
+    var allImages=document.getElementsByClassName('chart');
+    for (let i = 0; i < allImages.length; i++) {
+        allImages[i].style.display = 'none';
+    }    
+    var i = document.getElementById(which);
+    if (!i) return;
+    i.style.display='initial';
+}
